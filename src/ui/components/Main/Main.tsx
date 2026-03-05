0c55cc5a15d7d9800b86ebaddd1db4a7e3b10e71
dlubenchenko <d.lubenchenko@gmail.com>
2026-03-05 11:14:05 +0200

    feat(main): About, Experience components + Main.module.scss

---
+++ src/ui/components/Main/Main.tsx
@@ -0,0 +1,12 @@
+import About from "./About/About";
+import Experience from "./Experience/Experience";
+
+export function Main() {
+    return (
+        <div>
+            <About />
+            <Experience />
+        </div>
+    )
+}
+
